de48d79991549636d3f72957e9b8fae91aefa278
McVyp <93302780+McVyp@users.noreply.github.com>
2023-03-22 23:52:41 +0545

    updated site

--- src/consts.ts
+++ src/consts.ts
@@ -3,4 +3,4 @@
 
 export const SITE_TITLE = 'My personal website. -lye';
 export const SITE_DESCRIPTION = 'A journal of lye';
-export const HOMEPAGE_URL =  'https://www.lyejournal.dev';
+export const HOMEPAGE_URL =  'https://www.lyejournal.com/';
